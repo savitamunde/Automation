2168e120311663e8b7dfaec32e34b0b03e0c3521
savita <savitamunde@gmail.com>
2014-07-08 13:45:32 +0530

    new file has been add

---
+++ src/test/java/GitAuto/git/auto/SecondTest.java
@@ -0,0 +1,10 @@
+package GitAuto.git.auto;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+  @Test
+  public void secondTestMethod() {
+	  System.out.println("Second Test Case");
+  }
+}
